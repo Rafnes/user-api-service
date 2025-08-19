d66a4010e837a178b40bdd18089d1a3b27151212
Rafnes <rafzograf@gmail.com>
2025-08-19 19:11:37 +0300

    initial commit

---
+++ src/test/java/ru/skillsrock/user_api_service/UserApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillsrock.user_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
